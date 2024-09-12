0fe5430c1606e08240f8a2efbbaec4c2553d5662
Gustavo Santamaria <37514022+tavoohoh@users.noreply.github.com>
2024-09-12 22:42:09 +0000

    feat: product list update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import urllib.parse
+
+st.set_page_config(
+    page_title="Gustavo Santamaría | Venta de garage",
+    page_icon="https://tavoohoh.com/favicon.ico",
+    layout="centered"
+)
 
 products = [
     {
@@ -8,10 +15,28 @@ products = [
         "image": "https://images.unsplash.com/photo-1721332154161-847851ea188b?q=80&w=3135&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
-        "name": "IK Multimedia iRig Keys 2",
-        "price": 585000,
-        "description": "Nuevo / 3 teclas vinieron dañadas",
+        "name": "Set de raquetas (Artengo Tr110)",
+        "price": 160000,
+        "description": "Menos de un año de uso. Incluye bolso y pelotas",
         "image": "https://images.unsplash.com/photo-1724093121148-ec407f45e44c?q=80&w=3271&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        "name": "Bolso raqueta (Artengo 100M negro)",
+        "price": 65000,
+        "description": "Poco uso",
+        "image": "https://testimage-ima.com"
+    },
+    {
+        "name": "2 Raquetas de squash (Opfeel Sr100)",
+        "price": 80000,
+        "description": "Menos de un año de uso",
+        "image": "https://testimage-ima.com"
+    },
+    {
+        "name": "Chaqueta Berska",
+        "price": 250000,
+        "description": "Nueva. Comprada hace 2 años",
+        "image": "https://testimage-ima.com"
     }
 ]
 
@@ -23,8 +48,10 @@ st.logo(
 st.title("Venta de Garaje")
 st.markdown('<p style="font-size:18px;">Me mudé hace menos de un año y me di cuenta de que tengo muchas cosas que ya no necesito. Seguro que alguien más podría aprovecharlas mejor. Si estás interesado, escríbeme.</p>', unsafe_allow_html=True)
 
-whatsapp_link = "https://wa.me/573002220000"
-whatsapp_style = "border: 1px solid #fff; color: white; padding: 8px 12px; border-radius: 12px; cursor: pointer; text-decoration: none;"
+whatsapp_link = "https://wa.me/573002383905"
+whatsapp_style = "border: 1px solid #fff; color: white; padding: 4px 12px; border-radius: 12px; cursor: pointer; text-decoration: none; margin-top: 1.2rem; width: max-content; display: block;"
+
+st.divider()
 
 for product in products:
     col1, col2 = st.columns([1, 2])
@@ -33,11 +60,16 @@ for product in products:
         st.image(product["image"], use_column_width=True)
 
     with col2:
-        st.subheader(product["name"])
-        st.text(product['description'])
-        
         formatted_price = "${:,.0f} COP".format(product['price'])
-        st.write(f"**Precio**: {formatted_price}")
-        st.markdown(f'<a style="{whatsapp_style}" href="{whatsapp_link}" target="_blank">Comprar</a>', unsafe_allow_html=True)
-    
+        message = f"Hola, quisiera comprar {product['name']}"
+        encoded_message = urllib.parse.quote(message)
+
+        st.subheader(product["name"])
+
+        description = f'<p style="margin: 0;">{product["description"]}</p>'
+        price = f'<p><b>Precio:</b> {formatted_price}</p>'
+        btn = f'<a style="{whatsapp_style}" target="_blank" href="{whatsapp_link}?text={encoded_message}" target="_blank">Comprar</a>'
+
+        st.html(f'{description}{price}{btn}')
+
     st.divider()
